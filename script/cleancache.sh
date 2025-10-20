f7038b702080b0a72ee9a1ee26a95c2631962869
NorthZeroD <northzero_9376@outlook.com>
2025-10-21 07:59:19 +0800

    修改cleancache.sh

--- script/cleancache.sh
+++ script/cleancache.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# Recursively find and delete all __pycache__ directories in the src directory
 find src -type d -name "__pycache__" -exec rm -rf {} +
+
+echo Cleaned up __pycache__ directories.
